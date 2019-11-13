d6780ab39f174588d304723324f20bb45452d798
Mickael Durand <micdurand@gmail.com>
2019-11-13 16:38:48 +0100

    ex finished

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  RATINGS = [0, 1, 2, 3, 4, 5]
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: RATINGS }, numericality: { only_integer: true }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese',
+    phone_number:        '0505050505'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:        '0105050505'
+  },
+  {
+    name:         'MiamMiam',
+    address:      '22A rue du Miam, France',
+    category:  'french',
+    phone_number:        '0205050505'
+  },
+  {
+    name:         'Itadakimasu',
+    address:      '1 rue du soleil levant, à droite',
+    category:  'japanese',
+    phone_number:        '0305050505'
+  },
+  {
+    name:         'DesFritesDesFritesDesFrites',
+    address:      'chez les Tuches, dans le nord',
+    category:  'belgian',
+    phone_number:        '0405050505'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
